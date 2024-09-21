aa5022ba0a82e670e1ebc0d99ae287884bd64cd4
Benedikt Zundel <benedikt.zundel@live.com>
2024-09-21 06:28:50 +0200

    maybe this time?

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   outputs = { self }: {
     templates = {
       latex = {
-        path = self + "/not_latex";
+        path = ./latex;
         description = "Personalized LaTeX boilerplates";
       };
     };
